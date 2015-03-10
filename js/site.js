bddb40297c280372d2acb59a9b2b1c98fc938704
nuhuxi <mark.keckeis@gmail.com>
2015-03-10 09:28:41 -0700

    At end of modal actions

--- js/site.js
+++ js/site.js
@@ -1,14 +1,19 @@
 (function(){
   "use strict";
   
-  var pickButton = $('#pickButton');
+  var $pickButton = $('#pickButton');
   
   $("#reasonDropdown li a").on('click',  function(){
     var reason = $(this).text();      
-    $(pickButton).text(reason);
-                                
-                                
+    $pickButton.text(reason);                           
   });
   
+  var $sentDialog = $("#sentDialog");
+  
+  $sentDialog.on("hidden.bs.modal", function(){
+
+    alert("This could be JS that does something else - changes pages etc");
+
+  });
   
 })();
